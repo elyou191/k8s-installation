b3133c5571bab3fe336fb5c0e71d88c32a6a9e95
elyou191 <elyou191@gmail.com>
2022-05-24 16:29:16 +0000

    Update*

--- Install k8s.sh
+++ Install k8s.sh
@@ -1,3 +1,4 @@
+#/bin/bash
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
