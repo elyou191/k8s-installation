6efe553c54fadcc7f0ce78735f0d5f02b661e157
elyou191 <elyou191@gmail.com>
2022-05-24 16:34:12 +0000

    Update**

--- install_k8s.sh
+++ install_k8s.sh
@@ -6,3 +6,5 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+systemctl start kubelet
+kubeadm init
