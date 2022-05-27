81125e99eccfd4622f92e84c646e950d12aeb12c
elyou191 <elyou191@gmail.com>
2022-05-27 09:50:38 +0000

    k8s installation on Debian distribution with minikube

--- install_k8s_minikube.sh
+++ install_k8s_minikube.sh
@@ -6,6 +6,12 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo apt update
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-sudo apt-get install -y kubectl
-
+sudo useradd -mU username -s /bin/bash
+sudo passwd username
+sudo echo "username    ALL=(ALL:ALL) ALL" >> /etc/sudoers
+sudo chown -R /home/username
+su - username
+sudo usermod -aG docker $USER && newgrp docker
+sudo rm -rf /tmp/juju-mk*
+sudo rm -rf /tmp/minikube.*
+minikube start --driver=docker
